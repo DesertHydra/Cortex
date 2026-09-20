3ee18f468fd43648b481a445a6aded003b07c843
Up <10714589+UpcraftLP@users.noreply.github.com>
2026-09-20 08:07:26 +0200

    Use convention tags for ore feature generation

--- src/main/java/deserthydra/cortex/datagen/common/CortexFeatureProvider.java
+++ src/main/java/deserthydra/cortex/datagen/common/CortexFeatureProvider.java
@@ -5,12 +5,12 @@ import deserthydra.cortex.block.LapisFormationBlock;
 import deserthydra.cortex.block.RedstoneFormationBlock;
 import deserthydra.cortex.worldgen.CortexConfiguredFeatures;
 import deserthydra.cortex.worldgen.CortexPlacedFeatures;
+import net.fabricmc.fabric.api.tag.convention.v2.ConventionalBlockTags;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Vec3i;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.data.worldgen.features.OreFeatures;
-import net.minecraft.tags.BlockTags;
 import net.minecraft.util.Util;
 import net.minecraft.util.random.WeightedList;
 import net.minecraft.world.level.block.state.BlockState;
@@ -45,7 +45,7 @@ public class CortexFeatureProvider {
 				BiomeFilter.biome(),
 				BlockPredicateFilter.forPredicate(BlockPredicate.allOf(
 					BlockPredicate.ONLY_IN_AIR_OR_WATER_PREDICATE,
-					BlockPredicate.matchesTag(Vec3i.ZERO.below(), BlockTags.REDSTONE_ORES)
+					BlockPredicate.matchesTag(Vec3i.ZERO.below(), ConventionalBlockTags.REDSTONE_ORES)
 				))
 			)
 		));
@@ -59,7 +59,7 @@ public class CortexFeatureProvider {
 				BiomeFilter.biome(),
 				BlockPredicateFilter.forPredicate(BlockPredicate.allOf(
 					BlockPredicate.ONLY_IN_AIR_OR_WATER_PREDICATE,
-					BlockPredicate.matchesTag(Vec3i.ZERO.below(), BlockTags.LAPIS_ORES)
+					BlockPredicate.matchesTag(Vec3i.ZERO.below(), ConventionalBlockTags.LAPIS_ORES)
 				))
 			)
 		));
